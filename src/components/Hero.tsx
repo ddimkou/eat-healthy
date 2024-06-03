21800bb413ba5a049508a81779cc311082f87b59
ddimkou <ddimkou@outlook.com>
2024-06-03 14:42:04 +0300

    hero ui update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,13 +2,15 @@ import BlobAnimation from "./BlobAnimation";
 
 const Hero = () => {
   return (
-    <div className="blobContainer flex items-center justify-center xl:justify-between relative">
-      {/* text */}
-      <div className="text-cranberry-red xl:text-forest-green z-10 px-12 font-bold flex flex-col items-center justify-center relative">
-        <p className="text-5xl md:text-7xl">Eat Healthy</p>
-        <div className="w-full h-[24vh] md:h-[35vh] xl:hidden bg-logobg bg-cover bg-center"></div>
-        <div>
-          <p className="text-2xl md:text-3xl">Eat Clean, Live Green.</p>
+    <div className="blobContainer flex items-center justify-center xl:justify-between relative ">
+      {/* text w background */}
+      <div className="bg-white w-full px-12 py-6 z-10 ">
+        <div className="text-cranberry-red xl:text-forest-green font-bold flex flex-col items-start justify-center">
+          <p className="text-5xl md:text-7xl">Eat Healthy</p>
+          <div className="w-full h-[24vh] md:h-[35vh] xl:hidden bg-logobg bg-cover bg-center"></div>
+          <div>
+            <p className="text-2xl md:text-3xl ml-1">Eat Clean, Live Green.</p>
+          </div>
         </div>
       </div>
       <BlobAnimation />
