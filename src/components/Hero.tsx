d9db43af9db4438d0a94ebda484bb7dac3b38d11
ddimkou <ddimkou@outlook.com>
2024-06-04 23:48:25 +0300

    responsive hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,14 +2,14 @@ import BlobAnimation from "./BlobAnimation";
 
 const Hero = () => {
   return (
-    <div className="blobContainer flex items-center justify-center xl:justify-between relative ">
+    <div className="blobContainer flex flex-col xl:flex-row items-center justify-center xl:justify-between relative">
       {/* text w background */}
-      <div className="bg-white w-full px-12 py-6 z-10 ">
-        <div className="text-cranberry-red xl:text-forest-green font-bold flex flex-col items-start justify-center">
-          <p className="text-5xl md:text-7xl">Eat Healthy</p>
-          <div className="w-full h-[24vh] md:h-[35vh] xl:hidden bg-logobg bg-cover bg-center"></div>
+      <div className="xl:bg-white w-full px-12 py-6 z-10 flex flex-col items-center xl:items-start text-center xl:text-left">
+        <div className="text-cranberry-red xl:text-forest-green font-bold">
+          <p className=" text-4xl md:text-7xl">Eat Healthy</p>
+          <div className="w-full h-[32vh] md:h-[40vh] xl:hidden bg-logobg bg-cover bg-center my-4"></div>
           <div>
-            <p className="text-2xl md:text-3xl ml-1">Eat Clean, Live Green.</p>
+            <p className="text-xl md:text-3xl ml-1">Eat Clean, Live Green.</p>
           </div>
         </div>
       </div>
