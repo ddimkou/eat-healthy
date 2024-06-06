c965e3479fa11567dc76573f6153720398803768
ddimkou <ddimkou@outlook.com>
2024-06-06 12:47:22 +0300

    active navlink

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,16 @@ import Introduction from "./components/Introduction";
 import Navbar from "./components/Navbar";
 import NewsLetter from "./components/NewsLetter";
 import Home from "./components/Home";
+import About from "./components/About";
 
 const App = () => {
   return (
     <Router>
-      <div className="text-2xl text-red-400 font-montserrat">
+      <div className="text-2xl text-red-400 font-montserrat bg-eggshell-white">
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
           <Route path="/" element={<Introduction />} />
         </Routes>
         <NewsLetter />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,7 +11,9 @@ const Navbar = () => {
   return (
     <nav className="z-30 bg-white text-forest-green font-bold flex items-center justify-between px-4 md:px-16 shadow-md relative">
       {/* logo */}
-      <div className="bg-logobg bg-cover bg-center w-16 h-16 md:w-20 md:h-20"></div>
+      <NavLink to="/" className="flex items-center">
+        <div className="bg-logobg bg-cover bg-center w-16 h-16 md:w-20 md:h-20"></div>
+      </NavLink>
       {/* hamburger menu btn */}
       <div className="md:hidden flex items-center">
         <button
@@ -49,41 +51,77 @@ const Navbar = () => {
           isOpen ? "flex" : "hidden"
         } md:flex flex-col md:flex-row items-center md:items-start space-y-4 md:space-y-0 md:space-x-4 cursor-pointer text-sm absolute md:relative top-16 md:top-0 right-0 md:left-auto bg-white md:bg-transparent py-4 md:py-0 px-5 md:px-0 shadow-md md:shadow-none z-30`}
       >
-        <li
-          onClick={handleMenuItemClick}
-          className="hover:text-cranberry-red transition-colors duration-300"
-        >
-          <Link to="/">Home</Link>
+        <li onClick={handleMenuItemClick}>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive
+                ? "text-cranberry-red hover:text-cranberry-red transition-colors duration-300"
+                : "hover:text-meadow-green transition-colors duration-300"
+            }
+          >
+            Home
+          </NavLink>
         </li>
-        <li
-          onClick={handleMenuItemClick}
-          className="hover:text-cranberry-red transition-colors duration-300"
-        >
-          <Link to="/about">About</Link>
+        <li onClick={handleMenuItemClick}>
+          <NavLink
+            to="/about"
+            className={({ isActive }) =>
+              isActive
+                ? "text-cranberry-red hover:text-cranberry-red transition-colors duration-300"
+                : "hover:text-meadow-green transition-colors duration-300"
+            }
+          >
+            About
+          </NavLink>
         </li>
-        <li
-          onClick={handleMenuItemClick}
-          className="hover:text-cranberry-red transition-colors duration-300"
-        >
-          <Link to="/lifestyle">Lifestyle</Link>
+        <li onClick={handleMenuItemClick}>
+          <NavLink
+            to="/lifestyle"
+            className={({ isActive }) =>
+              isActive
+                ? "text-cranberry-red hover:text-cranberry-red transition-colors duration-300"
+                : "hover:text-meadow-green transition-colors duration-300"
+            }
+          >
+            Lifestyle
+          </NavLink>
         </li>
-        <li
-          onClick={handleMenuItemClick}
-          className="hover:text-cranberry-red transition-colors duration-300"
-        >
-          <Link to="/contact">Contact</Link>
+        <li onClick={handleMenuItemClick}>
+          <NavLink
+            to="/contact"
+            className={({ isActive }) =>
+              isActive
+                ? "text-cranberry-red hover:text-cranberry-red transition-colors duration-300"
+                : "hover:text-meadow-green transition-colors duration-300"
+            }
+          >
+            Contact
+          </NavLink>
         </li>
-        <li
-          onClick={handleMenuItemClick}
-          className="hover:text-cranberry-red transition-colors duration-300"
-        >
-          <Link to="/introduction">Introduction</Link>
+        <li onClick={handleMenuItemClick}>
+          <NavLink
+            to="/introduction"
+            className={({ isActive }) =>
+              isActive
+                ? "text-cranberry-red hover:text-cranberry-red transition-colors duration-300"
+                : "hover:text-meadow-green transition-colors duration-300"
+            }
+          >
+            Introduction
+          </NavLink>
         </li>
-        <li
-          onClick={handleMenuItemClick}
-          className="hover:text-cranberry-red transition-colors duration-300"
-        >
-          <Link to="/test">Test</Link>
+        <li onClick={handleMenuItemClick}>
+          <NavLink
+            to="/test"
+            className={({ isActive }) =>
+              isActive
+                ? "text-cranberry-red hover:text-cranberry-red transition-colors duration-300"
+                : "hover:text-meadow-green transition-colors duration-300"
+            }
+          >
+            Test
+          </NavLink>
         </li>
       </ul>
     </nav>
